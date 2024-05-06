9bb8a4cd52f2a8de3642756ab0b908a1c040002f
João Correia <joaocorreiaprof@gmail.com>
2024-05-06 23:41:17 +0100

    Started working on the game board

--- scripts.js
+++ scripts.js
@@ -11,3 +11,21 @@ for (row of matrix) {
   console.log(rowString);
 }
 */
+
+function createBoard() {
+  const row = 3;
+  const column = 3;
+  const board = [];
+
+  for (let i = 0; i < row; i++) {
+    board[i] = [];
+    for (let j = 0; j < column; j++) {
+      board[i][j] = "X"; // You can put any placeholder value here
+    }
+    console.log(i);
+    console.log(board[i]); // Now you'll see the content of each row
+  }
+  console.log(board);
+}
+
+createBoard();
