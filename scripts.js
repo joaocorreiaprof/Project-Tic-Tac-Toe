9de8114b50a980eefe7349892911c17593c2d2af
João Correia <joaocorreiaprof@gmail.com>
2024-05-11 17:12:23 +0100

    Added html elements and created Players object

--- scripts.js
+++ scripts.js
@@ -1,17 +1,3 @@
-/*
-E.g: 2d Matrix
-const matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
-
-for (row of matrix) {
-  const rowString = row.join(" ");
-  console.log(rowString);
-}
-*/
-
 function createBoard() {
   const row = 3;
   const column = 3;
@@ -22,14 +8,32 @@ function createBoard() {
     for (let j = 0; j < column; j++) {
       board[i][j] = " ";
     }
-    console.log(i);
-    console.log(board[i]);
   }
-
-  board[0][0] = "1";
-  board[0][1] = "2";
-  board[0][2] = "3";
   console.log(board);
 }
 
-createBoard();
+const Players = {
+  player1: {
+    name: "Player 1",
+    mark: "X",
+  },
+  player2: {
+    name: "Player 2",
+    mark: "O",
+  },
+
+  getPlayersMark: function () {
+    const playerArray = Object.values(this);
+    const playerMarks = playerArray.map((player) => player.mark);
+
+    console.log(playerMarks);
+    return playerMarks;
+  },
+};
+
+function controlFlow() {
+  createBoard();
+  takeTurn();
+}
+
+controlFlow();
